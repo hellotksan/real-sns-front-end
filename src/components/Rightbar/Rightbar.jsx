a1716762e28b57a953b8f071132d4c2e0e7a0ee4
siwTakahasi <147582192+hellotksan@users.noreply.github.com>
2024-05-14 23:15:44 +0900

    incident fixing axios module installing

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Rightbar.css";
+import axios from "axios";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
