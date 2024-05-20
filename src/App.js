e35c51f77ae678d2619e2f035104575f10514761
siwTakahasi <147582192+hellotksan@users.noreply.github.com>
2024-05-20 22:02:53 +0900

    editpost pages and components adding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 import Setting from "./pages/settings/Settings";
+import PostEdit from "./pages/postEdit/PostEdit";
 import {
   BrowserRouter as Router,
   Route,
@@ -14,47 +15,19 @@ import { AuthContext } from "./state/AuthContext";
 
 function App() {
   const { user } = useContext(AuthContext);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (user !== undefined) {
-      setLoading(false);
-    }
-  }, [user]);
 
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={loading ? <p>Loading...</p> : user ? <Home /> : <Register />}
-        />
-        <Route
-          path="/login"
-          element={
-            loading ? <p>Loading...</p> : user ? <Navigate to="/" /> : <Login />
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            loading ? (
-              <p>Loading...</p>
-            ) : user ? (
-              <Navigate to="/" />
-            ) : (
-              <Register />
-            )
-          }
-        />
-        {/* <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/" element={user ? <Home /> : <Register />} />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
         <Route
           path="/register"
           element={user ? <Navigate to="/" /> : <Register />}
-        /> */}
+        />
         <Route path="/profile/:username" element={<Profile />} />
         <Route path="/settings/:username" element={<Setting />} />
+        <Route path="/postedit/:postId" element={<PostEdit />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/postEdit/PostEdit.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import EditPost from "../../components/EditPost/EditPost";
+import "./PostEdit.css";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+function PostEdit() {
+  const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const [user, setUser] = useState({});
+  const { username } = useParams();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          PUBLIC_FOLDER + `/api/users?username=${username}`
+        );
+        setUser(response.data);
+        console.log("Success fetching user data!", user);
+      } catch (error) {
+        console.log("Error fetching user data.", error);
+      }
+    };
+    fetchUser();
+  }, [username, PUBLIC_FOLDER]);
+
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <EditPost />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PostEdit;
